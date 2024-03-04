6a94cfce785d4fdc82a5a40edfa864564f807287
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-03-05 00:56:16 +0300

    ✅ Done

--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -1,12 +1,11 @@
 from django.template import loader
-from django.shortcuts import redirect,render
+from django.shortcuts import redirect, render
 from django.http import HttpRequest
 
 
 def home_page(request: HttpRequest):
-    # template = loader.get_template('index.html')
 
-    return render(request,"index.html")
+    return render(request, "index.html")
 
 
 def mode_light(request: HttpRequest):
@@ -25,13 +24,21 @@ def mode_dark(request: HttpRequest):
 
 
 def properties_page(request: HttpRequest):
-   
-    return render(request, "properties.html")
+    properties = [
+        {"title": "Villa Modern in Malqa", "image": "villa1.jpg"},
+        {"title": "Great home for you in Rimal", "image": "villa2.jpg"},
+        {"title": "Villa with 8 bedrooms in Swedey", "image": "villa3.jpg"},
+        {"title": "Amazing Villa in Hitten", "image": "villa4.jpg"},
+        {"title": "Amazing Villa in Hitten", "image": "villa4.jpg"},
+    ]
+    context = {"properties": properties}
+
+    return render(request, "properties.html", context)
 
 
 def about_page(request: HttpRequest):
-   
-    return render(request,"about.html")
+
+    return render(request, "about.html")
 
 
 def contactus_page(request: HttpRequest):
