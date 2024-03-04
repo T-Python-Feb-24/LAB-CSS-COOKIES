141b595a1b1f5390da254b697721fdcd5c3eb351
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-03-04 15:05:13 +0300

    Done home

--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -1,22 +1,38 @@
 from django.template import loader
-from django.http import HttpResponse, HttpRequest
+from django.shortcuts import redirect,render
+from django.http import HttpRequest
 
 
 def home_page(request: HttpRequest):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    # template = loader.get_template('index.html')
+
+    return render(request,"index.html")
+
+
+def mode_light(request: HttpRequest):
+
+    response = redirect("main:home_page")
+    response.set_cookie("mode", "False")
+
+    return response
+
+
+def mode_dark(request: HttpRequest):
+    response = redirect("main:home_page")
+    response.set_cookie("mode", "True")
+
+    return response
 
 
 def properties_page(request: HttpRequest):
-    template = loader.get_template('properties.html')
-    return HttpResponse(template.render())
+   
+    return render(request, "properties.html")
 
 
 def about_page(request: HttpRequest):
-    template = loader.get_template('about.html')
-    return HttpResponse(template.render())
+   
+    return render(request,"about.html")
 
 
 def contactus_page(request: HttpRequest):
-    template = loader.get_template('contactus.html')
-    return HttpResponse(template.render())
+    return render(request, "contactus.html")
