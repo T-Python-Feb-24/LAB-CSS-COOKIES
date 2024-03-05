d7b4ed04ab607c68b7d36bd3c85cc60ebd24f5ca
Abdullah <.alhumidi.Ab@gmail.com>
2024-03-05 10:06:02 +0300

    adding

--- RealEstate/myapp/urls.py
+++ RealEstate/myapp/urls.py
@@ -5,7 +5,8 @@ app_name = "myapp"
 
 urlpatterns = [
     path('', views.home_view, name='home'), 
-    path('properties/', views.home, name='properties'),
-    path("mode/dark/", views.mode_dark, name="mode_large_view"),
-    path("mode/light/", views.mode_light, name="mode_small_view"),
+    path('properties/', views.properties_view, name='properties'),
+    path("", views.properties_view, name="properties"),
+    path('mode-dark/', views.mode_dark, name='mode_dark'), 
+    path('mode_light/', views.mode_light, name='mode_light'),  
 ]
--- RealEstate/myapp/views.py
+++ RealEstate/myapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
-
 # Create your views here.
 
 def home_view(request:HttpRequest):
@@ -12,27 +11,25 @@ def home_view(request:HttpRequest):
 
 #    return render(request, "main/properties.html")
 
-def home(request):
+def properties_view(request):
+    # List of properties with title and image
     properties = [
         {"title": "Villa Modern in Malqa", "image": "villa1.jpg"},
         {"title": "Great home for you in Rimal", "image": "villa2.jpg"},
         {"title": "Villa with 8 bedrooms in Swedey", "image": "villa3.jpg"},
         {"title": "Amazing Villa in Hitten", "image": "villa4.jpg"},
     ]
-    return render(request, "main/Properties.html", {'properties': properties})
-def explore_properties(request):
-    pass
-def mode_dark(request:HttpRequest):
+    # Render the "Properties.html" template with properties data
+    return render(request, "myapp/Properties.html", {'properties': properties})
 
+
+
+def mode_dark(request: HttpRequest) -> HttpResponse:
     response = redirect("main:home_view")
     response.set_cookie("Dark_mode", "True", max_age=60*60*24*7)
-
     return response
 
-
-def mode_light(request:HttpRequest):
-
-    response = redirect("main:home_view")
-    response.set_cookie("light_mode", "False")
-
-    return response
+def mode_light(request: HttpRequest) -> HttpResponse:
+    response = redirect("myapp:home_view")
+    response.set_cookie("Dark_mode", "False")  # Change to "Dark_mode"
+    return render(request, "myapp/home.html")
