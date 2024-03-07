b9bc246acb3a2b8029b30bcc7c1da06f89b7b1b5
Shekah <shekhahathlol@gmail.com>
2024-03-07 11:23:31 +0300

    second commit

--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 # Create your views here.
 
+properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+    ]
 def home_page(request:HttpRequest):
     return render(request,'main/home_page.html')
 
 def properties_page(request:HttpRequest):
-    return render(request,'main\properties.html')
+    context = {'properties':properties}
+    return render(request,'main\properties.html',context)
 
 def contact_page(request:HttpRequest):
     return render(request,'main/contact.html')
